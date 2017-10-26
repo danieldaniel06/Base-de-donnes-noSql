0eeab063337124062cc1ba8881dd776faba20a45
lionda <daniel.ahmedinfo@gmail.com>
2017-10-26 18:17:10 +0200

    2 commit

--- app/read_write.js
+++ app/read_write.js
@@ -1,5 +1,11 @@
-var installation_file = '/Users/danielahmed/Desktop/noSQLProject/app/jsonFiles/installations.json';
-var installation_file_write = '/Users/danielahmed/Desktop/noSQLProject/app/jsonFiles/installations_w.json';
+var installation_file = '/Users/danielahmed/Desktop/jsonFiles/installations.json';
+var installation_file_write = '/Users/danielahmed/Desktop/jsonFiles/installations_w.json';
+
+var equipement_file = '/Users/danielahmed/Desktop/jsonFiles/equipements.json';
+var equipement_file_write = '/Users/danielahmed/Desktop/jsonFiles/equipements_w.json';
+
+var activite_file = '/Users/danielahmed/Desktop/jsonFiles/activites.json';
+var activite_file_write = '/Users/danielahmed/Desktop/jsonFiles/activites_w.json';
 
 readFile = function(file) {
 	var fs = require('fs');
@@ -12,5 +18,11 @@ writeFile = function(file, data) {
 	fs.writeFileSync(file, data);
 };
 
-var data = JSON.parse(readFile(installation_file));
-writeFile(installation_file_write, JSON.stringify(data.installations));
+var dataI = JSON.parse(readFile(installation_file));
+writeFile(installation_file_write, JSON.stringify(dataI.installations));
+
+var dataE = JSON.parse(readFile(equipement_file));
+writeFile(equipement_file_write, JSON.stringify(dataE.equipements));
+
+var dataA = JSON.parse(readFile(activite_file));
+writeFile(activite_file_write, JSON.stringify(dataA.activites));
